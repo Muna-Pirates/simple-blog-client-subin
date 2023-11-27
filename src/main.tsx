74c56ef4e5a4db96879f1824c5dd209f99baeb8a
Subin Ahn <51507260+anxiubin@users.noreply.github.com>
2023-11-27 15:40:34 +0900

    :sparkles: Setup apollo/client and graphql

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import { ApolloProvider } from "@apollo/client"
+import ApolloClient from "./lib/client/apollo"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<ApolloProvider client={ApolloClient}>
+		<App />
+	</ApolloProvider>
 )
