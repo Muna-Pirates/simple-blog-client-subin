74240c4eb202a4be8cfb887573ab92121db44286
Subin Ahn <51507260+anxiubin@users.noreply.github.com>
2023-12-01 18:04:21 +0900

    :bug: Add button handler on RegisterComplete page

--- src/features/auth/pages/register-complete/RegisterComplete.tsx
+++ src/features/auth/pages/register-complete/RegisterComplete.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/ui/button"
 import Center from "@/layout/center/Center"
 import { CheckCircleFilled } from "@ant-design/icons"
+import { useNavigate } from "react-router-dom"
+
 const RegisterComplete = () => {
+	const navigate = useNavigate()
+	const handleClickGoLogin = () => {
+		navigate("/login")
+	}
 	return (
 		<Center>
 			<h1 className="text-3xl font-bold mb-8">REGISTRATION COMPLETED</h1>
 			<CheckCircleFilled style={{ fontSize: 40, color: "green" }} />
-			<Button className="mt-8 text-lg">Continue</Button>
+			<Button className="mt-8 text-lg" onClick={handleClickGoLogin}>
+				Continue
+			</Button>
 		</Center>
 	)
 }
