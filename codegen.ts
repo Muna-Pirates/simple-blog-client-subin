8a61323748cb64ed0d90ea01ac831a704a2f687f
Subin Ahn <51507260+anxiubin@users.noreply.github.com>
2023-12-13 10:21:02 +0900

    :wrench: Update API base url

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://ec2-3-34-193-123.ap-northeast-2.compute.amazonaws.com:3000/graphql',
+  schema: "https://simple-blog.munawiki.dev/graphql",
   documents: ['src/**/*.{ts,tsx,graphql}'],
   generates: {
     './src/lib/graphql/': {
