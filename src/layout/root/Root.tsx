227c353b1607c48194d8889875980ded7f452d9f
Subin Ahn <51507260+anxiubin@users.noreply.github.com>
2023-12-22 11:52:31 +0900

    :sparkles: Add darkmode: using ThemeProvider

--- src/layout/root/Root.tsx
+++ src/layout/root/Root.tsx
@@ -1,6 +1,7 @@
 import { AlertDialog } from "@/components/ui/alert-dialog"
 import Center from "../center/Center"
 import Header from "./header/Header"
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 interface IProps {
 	children: React.ReactNode
@@ -8,12 +9,14 @@ interface IProps {
 
 const Root = ({ children }: IProps) => {
 	return (
-		<AlertDialog>
-			<div className="py-4 px-4 sm:px-8 flex flex-nowrap flex-col h-full my-0 mx-auto">
-				<Header />
-				<Center>{children}</Center>
-			</div>
-		</AlertDialog>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<AlertDialog>
+				<div className="py-4 px-4 sm:px-8 flex flex-nowrap flex-col h-full my-0 mx-auto">
+					<Header />
+					<Center>{children}</Center>
+				</div>
+			</AlertDialog>
+		</ThemeProvider>
 	)
 }
 
